2ce27a25bb95e9ff05595998e1cf1d560d8b0b06
Inna Lazorko <inna-h@i.ua>
2022-10-20 16:22:26 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <button>Add contact</button>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
