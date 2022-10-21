e775996a0ba5270e69fd468b2a48ef9615bd49f0
Inna Lazorko <inna-h@i.ua>
2022-10-21 16:39:35 +0300

    done

--- src/components/contactsForm/ContactsForm.jsx
+++ src/components/contactsForm/ContactsForm.jsx
@@ -55,7 +55,7 @@ export class ContactsForm extends Component {
             type="text"
             name="name"
             value={name}
-            placeholder="Name"
+            placeholder="Rosie Simpson"
             onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -70,7 +70,7 @@ export class ContactsForm extends Component {
             type="tel"
             name="number"
             value={number}
-            placeholder="Number"
+            placeholder="459-12-56"
             onChange={this.handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
